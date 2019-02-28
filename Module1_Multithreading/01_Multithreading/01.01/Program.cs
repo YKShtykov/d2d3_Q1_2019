931ae5065153c39ee1197f8aeb792c349ec4f648
Yakov Shtykov <yakov.shtykov@gmail.com>
2019-02-28 12:59:29 +0300

    Two tasks completed

--- Module1_Multithreading/01_Multithreading/01.01/Program.cs
+++ Module1_Multithreading/01_Multithreading/01.01/Program.cs
@@ -2,11 +2,27 @@
 
 namespace _01
 {
+    using System.Threading.Tasks;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var taskArray = new Task[100];
+
+            for (int i = 0; i < taskArray.Length; i++)
+            {
+                taskArray[i] = Task.Factory.StartNew((taskNum) =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        Console.WriteLine($"Task #{taskNum} – {j}");
+                    }
+                }, i);
+            }
+
+            Task.WaitAll(taskArray);
+            Console.ReadKey();
         }
     }
 }
--- Module1_Multithreading/01_Multithreading/01.02/Program.cs
+++ Module1_Multithreading/01_Multithreading/01.02/Program.cs
@@ -2,11 +2,51 @@
 
 namespace _02
 {
+    using System.Linq;
+    using System.Threading.Tasks;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task.Run(() =>
+            {
+                var array = new int[10];
+                var rand = new Random();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = rand.Next(0, 100);
+                }
+                Console.WriteLine($"Array: {string.Join(',',array)}");
+                return array;
+
+            })
+            .ContinueWith((prevTask) =>
+            {
+                int[] array = prevTask.Result;
+                var randMultiplier = new Random().Next(0, 100);
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = array[i] * randMultiplier;
+                }
+                Console.WriteLine($"Random multiplier: {randMultiplier}. Array: {string.Join(',', array)}");
+                return array;
+            })
+            .ContinueWith(prevTask =>
+            {
+                var list = prevTask.Result.ToList();
+                list.Sort();
+                Console.WriteLine($"Sorted array: {string.Join(',', list)}");
+                return list;
+
+            })
+            .ContinueWith((prevTask) =>
+            {
+                var list = prevTask.Result;
+                Console.WriteLine($"Average: {list.Average()}");
+            });
+
+            Console.ReadKey();
         }
     }
 }
