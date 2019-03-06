a1fe8534c2c859a899221e1746dc8c9d060cab53
Yakov Shtykov <yakov.shtykov@gmail.com>
2019-03-06 18:03:20 +0300

    2.2 2.3 2.4 tasks added

---
+++ Module1_Multithreading/02_AsyncAwait/02.04/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02._04.Controllers
+{
+    using Model;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly EntityRepository _repository = new EntityRepository();
+
+        // GET: api/Product
+        [HttpGet]
+        public async Task<IEnumerable<Entity>> GetAsync()
+        {
+            return await _repository.GetAsync();
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public async Task PostAsync([FromBody] Entity entity)
+        {
+            await _repository.PostAsync(entity);
+        }
+
+        // PUT: api/Product/5
+        [HttpPut("{id}")]
+        public async Task PutAsync([FromBody] Entity entity)
+        {
+
+            await _repository.PostAsync(entity);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public async Task DeleteAsync(Entity entity)
+        {
+            await _repository.PostAsync(entity);
+        }
+    }
+}
---
+++ Module1_Multithreading/02_AsyncAwait/02.04/Model/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _02._04.Model
+{
+    public class Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
